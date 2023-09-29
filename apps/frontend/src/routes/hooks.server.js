79dc94ee3a70a315653c118c28b636845936cfaf
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-09-29 13:29:11 -0700

    Added homepage and register page with server load functions

--- apps/frontend/src/routes/hooks.server.js
+++ apps/frontend/src/routes/hooks.server.js
@@ -1,18 +1,28 @@
 import PocketBase from 'pocketbase';
 import { serializeNonPOJOs } from '../lib/utils';
 
+// Hooks are run on every request on our Sveltekit app.
+
 export const handle = async ({ event, resolve }) => {
+	// This hook initiates a new Pocketbase instance.
 	event.locals.pb = new PocketBase('http://127.0.0.1:8090/');
+
+	// Grabs the cookie from the browser, if we have one/ Checks it and loads it in our Pocketbase's authStore so we can use it.
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
+	// If it is valid, serializes the model and sets it to our user.
+	//If it's not valid, sets the user to undefined.
 	if (event.locals.pb.authStore.isValid) {
 		event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
 	} else {
 		event.locals.user = undefined;
 	}
 
+	// Everything that happens server-side, takes place under the await resovlve event.
 	const response = await resolve(event);
 
+	// Exports the latest authStore to the cookie in the browser,
+	// so when a request is done, we have the latest instance of the authStore.
 	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
 
 	return response;
