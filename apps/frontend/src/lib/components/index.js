c632e2a195213f6892ed02cd180c256e576326d2
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-10-02 10:47:28 +0200

    Update index.js

--- apps/frontend/src/lib/components/index.js
+++ apps/frontend/src/lib/components/index.js
@@ -1,3 +1,5 @@
+// Exporting all components from this file, so that we can import them easier
+
 import Input from './Input.svelte';
 import Modal from './Modal.svelte';
 import MyProjectItem from './MyProjectItem.svelte';
